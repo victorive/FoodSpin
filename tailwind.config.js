ac1472fc533011f661c19969df10153377c99d03
OVI <victorogugua44@gmail.com>
2022-03-10 20:14:21 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./public/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        '3xl': "0px 20px 40px rgba(244, 226, 209, 1)",
+      },
+    },
+  },
+  plugins: [],
+}
